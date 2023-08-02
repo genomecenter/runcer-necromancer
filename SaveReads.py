0a11fa4b98edd38a9e41e40663c165cf024a2311
Zenerzul <olegsuchalko@gmail.com>
2023-08-02 13:10:32 +0300

    Modified arguments
    Script was modified with argparse. Now two modes are available to work with (SE or PE run mode).

--- SaveReads.py
+++ SaveReads.py
@@ -1,11 +1,34 @@
-import sys, os
+import argparse
+import os
 from subprocess import check_call
 
-if __name__ == "__main__":
+
+def main(args):
     files_list = []
+    if args.run_type == "SE":
+        file_ending = ".fq.gz"
+    elif args.run_type == "PE":
+        file_ending = "_1.fq.gz"
+    
     for filename in os.listdir():
-        if filename.endswith("_1.fq.gz"):
+        if filename.endswith(file_ending):
             files_list.append(filename)
 
     exec_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "SaveReads")
-    check_call([exec_path, sys.argv[1]]+files_list)
+    check_call([exec_path, args.fq_file]+files_list)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "run_type",
+        metavar="run_type", 
+        choices=["SE", "PE"],
+        help="Define run type"
+    )
+    parser.add_argument(
+        "fq_file", 
+        help="Path to non-demultiplexed fastq-file (must be ungzipped)"
+    )
+    args = parser.parse_args()
+    main(args)
